e7c8173b5142c77fc44ef0cb755afbffcd177eba
Mateus-de-Morais-Barros <mateusdemorais12345@gmail.com>
2025-11-25 14:58:59 -0300

    feat: adiciona testes de auth

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test'
-import { type } from 'os'
 
 test.describe("Auth - CreateToken", () => {
     test("Validar criação de token com Sucesso", async ({ request })=>{
@@ -18,5 +17,89 @@ test.describe("Auth - CreateToken", () => {
         expect(body).toHaveProperty('token')
         expect(typeof body.token).toBe('string')
         expect(body.token).not.toHaveLength(0)
+    })
+
+    test("Tentativa de criação de token com credenciais inválidas", async ({request})=>{
+        const username = "usererrado"
+        const password = "senhaerrada"
+        const response = await request.post('/auth', {
+            data: {
+                username: username,
+                password: password
+            }
+        })
+
+        const body = await response.json()
+        console.log((response))
+        // expect(response.status()).toBe(401)
+        expect(body).toHaveProperty("reason")
+        expect(body.reason).toBe("Bad credentials")
+    })
+
+    test("Tentativa de criação de token com usuário inválido", async ({ request }) => {
+        const username = "usererrado"
+        const password = "password123"
+        const response = await request.post('/auth', {
+            data: {
+                username: username,
+                password: password
+            }
+        })
+
+        const body = await response.json()
+        console.log((response))
+        // expect(response.status()).toBe(401)
+        expect(body).toHaveProperty("reason")
+        expect(body.reason).toBe("Bad credentials")
+    })
+
+    test("Tentativa de criação de token com senha inválida", async ({ request }) => {
+        const username = "admin"
+        const password = "senhaerrada"
+        const response = await request.post('/auth', {
+            data: {
+                username: username,
+                password: password
+            }
+        })
+
+        const body = await response.json()
+        console.log((response))
+        // expect(response.status()).toBe(401)
+        expect(body).toHaveProperty("reason")
+        expect(body.reason).toBe("Bad credentials")
+    })
+
+    test("Tentativa de criação de token com redenciais em branco", async ({ request }) => {
+        const username = ""
+        const password = ""
+        const response = await request.post("/auth", {
+            data: {
+                username: username,
+                password: password
+            }
+        })
+
+        const body = await response.json()
+
+        // expect(response.status()).toBe(401)
+        expect(body).toHaveProperty("reason")
+        expect(body.reason).toBe("Bad credentials")    
+    })
+
+    test("Tentativa de criação de token com content-type incorreto", async ({ request }) => {
+        const username = ""
+        const password = ""
+        const response = await request.post("/auth", {
+            headers: {
+                "Content-Type":"application/xml",
+            },
+            data: {
+                username: username,
+                password: password
+            }
+        })
+
+        expect(response.status()).toe(400)
     })
 })
