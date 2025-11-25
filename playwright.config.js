e96ac5139849b6fe32c56ab58802f572c4b764e9
Mateus-de-Morais-Barros <mateusdemorais12345@gmail.com>
2025-11-25 17:29:23 -0300

    Change url in config

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
   reporter: 'line',
 
   use: {
-    baseURL: process.env.CI ? 'https://restful-booker.herokuapp.com' :'http://localhost:3001',
+    // baseURL: process.env.CI ? 'https://restful-booker.herokuapp.com' :'http://localhost:3001',
+    baseURL: 'https://restful-booker.herokuapp.com',
     trace: 'on-first-retry',
   },
 
